83807c2f8216986c29e6a0d5722f52b0016b0d19
Luap485 <geiser.paul@gmail.com>
2023-06-22 12:14:03 +0200

    1version

---
+++ Angesetllter.java
@@ -0,0 +1,20 @@
+public class Angestellter extends Person{
+    private KUNDE aktKunde;
+    private BANK bank;
+    private Konto aktKonto;
+
+    int SparkontoEinrichten(double){}
+    ANGESTELLTER(String, int, BANK){}
+    int SparkontoEinrichten(double){}
+    int GirokontoEinrichten(double){}
+
+    boolean KantoLoeschen0){}
+    String AngestelltenLoeschen(String){}
+
+    AUSZUGSEINTRAG AuszugsdatenErstelen){}
+    boolean KundeWaehlen(String){}
+    String Neuenkunden Einrichten(String, int ){}
+    KUNDE AktkundeGebent(){}
+    void KontoSetzen(int){}
+    String KundenLoeschen(){}
+    String NeuenAngestellterEinrichten(String, int){}
---
+++ Kunde.java
@@ -0,0 +1,3 @@
+public class Kunde extends Person {
+    Kunde aktKonto;
+}
