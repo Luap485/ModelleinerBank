cb75d05f8a5910342945fcc90b7fbe71c1a376e1
Luap485 <geiser.paul@gmail.com>
2023-06-27 12:03:22 +0200

    1version

--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,10 @@
 public abstract class Person {
     String Name;
     int Pin;
-    public Person (int newPin, String newName) {
-        this.Name= newName;
-        this.Pin=newPin;
+
+    public Person(int newPin, String newName) {
+        this.Name = newName;
+        this.Pin = newPin;
     }
 
 }
