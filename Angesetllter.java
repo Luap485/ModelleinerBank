cb756e41e50c99e71a543fda842f24691f2541f9
Luap485 <geiser.paul@gmail.com>
2023-06-27 12:03:22 +0200

    1version

--- Angesetllter.java
+++ Angesetllter.java
@@ -1,20 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Angestellter extends Person{
     private KUNDE aktKunde;
     private BANK bank;
     private Konto aktKonto;
+    public Angestellter [] array AngesetlltenListe;
+    public Angestellter(){
+        this.
 
-    int SparkontoEinrichten(double){}
+    }
     ANGESTELLTER(String, int, BANK){}
     int SparkontoEinrichten(double){}
     int GirokontoEinrichten(double){}
 
-    boolean KantoLoeschen0){}
+    boolean KontoLoeschen(){}
     String AngestelltenLoeschen(String){}
 
-    AUSZUGSEINTRAG AuszugsdatenErstelen){}
+    AUSZUGSEINTRAG AuszugsdatenErstellen(){}
     boolean KundeWaehlen(String){}
     String Neuenkunden Einrichten(String, int ){}
     KUNDE AktkundeGebent(){}
     void KontoSetzen(int){}
     String KundenLoeschen(){}
     String NeuenAngestellterEinrichten(String, int){}
+    }
