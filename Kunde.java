a9625afc6dc023d523435699dcb30fe198649970
Luap485 <geiser.paul@gmail.com>
2023-07-05 18:42:34 +0200

    1version

--- Kunde.java
+++ Kunde.java
@@ -1,10 +1,51 @@
-    public class Kunde extends Person {
+public class Kunde extends Person {
         Kunde aktKonto;
 
-        KUNDE(String, int){}
-        double KontostandGeben() void Einzahlen(double)
-        boolean Abheben(double).
-        AUSZUGSEINTRAG AuszugsdatenErstellen()
-        boolean AktkontoSetzen(int)
+        private Kunde aktKonto;
+        private double kontostand;
 
-    }
+        KUNDE(String name, int id){
+                this.name = name;
+                this.id = id;}
+
+        public String getName() {
+                return name;}
+
+        double KontostandGeben(){
+                return kontostand;
+        }
+
+        void Einzahlen(double Menge){
+                kontostand += Menge;
+                System.out.println("Erfolgreich Eingezahlt: " + Menge)
+        }
+
+        boolean Abheben(double){
+                if (Menge <= kontostand) {
+                        kontostand -= Menge;
+                        System.out.println("Erfolgreich abgehoben: " + Menge);
+                        return true;
+                } else {
+                        System.out.println("Kein Geld vorhanden.");
+                        return false;
+                }
+        }
+        //Müssen wir noch machen
+        AUSZUGSEINTRAG AuszugsdatenErstellen(){
+                //accountStatement.Auszugsdaten("Datum: 2023-06-28");
+                //accountStatement.Auszugsdaten("Konto Besitzer: " + getName());
+                // accountStatement.Auszugsdaten("Kontostand: " + getKontostand());
+
+                return accountStatement;
+        }
+
+        boolean AktkontoSetzen(int Kontonummer){
+
+                if (Kontonummer == 12345) {
+                        AktKonto = this;
+                        return true;
+                } else {
+                        return false;
+                }
+        }
+}
